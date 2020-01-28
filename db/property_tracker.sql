674bf582ef7ae30c07632839e5e03cc3ac4bc630
lukerhys <hello@lukemelvin.com>
2020-01-28 14:55:30 +0000

    Creation of sql file.

---
+++ db/property_tracker.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS properties;
+
+CREATE TABLE properties (
+  number_of_bedrooms VARCHAR(255),
+  year_built VARCHAR(255),
+  status VARCHAR(255),
+  build VARCHAR(255),
+  id SERIAL PRIMARY KEY
+);
