97bbc061eb64a49dc5f4d2b76124bca61fb040da
lukerhys <hello@lukemelvin.com>
2020-01-28 15:54:18 +0000

    created delete one function.

--- models/property_tracker.rb
+++ models/property_tracker.rb
@@ -27,6 +27,18 @@ end
     db.close()
   end
 
+    def delete()
+      db = PG.connect({
+      dbname: "property_tracker",
+      host: "localhost"
+    })
+    sql = "DELETE FROM properties WHERE id = $1"
+    values = [@id]
+    db.prepare("delete_one", sql)
+    db.exec_prepared("delete_one", values)
+    db.close()
+    end
+
   def Property.all()
     db = PG.connect({
       dbname: "property_tracker",
