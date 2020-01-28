b742548184d93d615fc05d8271e6cfd8b8202bfe
lukerhys <hello@lukemelvin.com>
2020-01-28 15:18:29 +0000

    Creation of save function

--- console.rb
+++ console.rb
@@ -2,6 +2,7 @@ require ("pg")
 
 class Property
 
+attr_accessor :number_of_bedrooms, :year_built, :status, :build
 
 def initialize(options)
   @id = options["id"].to_i() if options["id"]
@@ -9,9 +10,21 @@ def initialize(options)
 @year_built=options["year_built"]
 @status=options["status"]
 @build=options["build"]
-
 end
 
+  def save()
+db=PG.connect({
+    dbname: "property_tracker",
+    host:"localhost"
+  })
+  sql="INSERT INTO properties
+    (number_of_bedrooms,year_built,status,build) VALUES
+    ($1,$2,$3,$4) RETURNING id"
+    values=[@number_of_bedrooms,@year_built,@status,@build]
+    db.prepare("save",sql)
+    @iq=db.exec_prepared("save",values).first()["id"].to_i
+    db.close()
+  end
 
 
 
